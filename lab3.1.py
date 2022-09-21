38e2ee7f047ce7ba3580a53b4d8136a4f69f3f84
OlexanderChayka <113577493+OlexanderChayka@users.noreply.github.com>
2022-09-21 22:31:59 +0300

    Add files via upload

---
+++ lab3.1.py
@@ -0,0 +1,14 @@
+print('Введіть 1 число')
+print('Введіть 2 число')
+while True:
+    
+    n=int(input())
+    m=int(input())
+    n1=0
+    while n > 0 :
+     n1+= n % 10
+     n=n // 10
+     while m > 0 :
+      n1+= m % 10
+      m=m // 10
+    print (n1)
