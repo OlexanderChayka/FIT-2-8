143fe41d1ea51e907515e61ceaf4ee5a71d3ad17
Olexandr Chayka <113577493+OlexanderChayka@users.noreply.github.com>
2023-09-27 22:50:08 +0300

    Delete lab4.2.py

--- lab4.2.py
+++
@@ -1,9 +0,0 @@
-import math
-
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input())
-x1 = float(input())
-x = math.sqrt(a*math.cos(b)+math.e**c)+math.log(2**x1+4, 7)
-
-print(f"x = {x}")
